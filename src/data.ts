e57703d4f695a6faefc677796965434ab264537c
liampuk <liam.piesley@marshmallow.co>
2025-02-22 00:49:27 +0000

    minikeys demo project

---
+++ src/data.ts
@@ -0,0 +1,124 @@
+import { Sample } from "minikeys"
+
+export const samples: Sample[] = [
+  {
+    note: "C1",
+    url: "/samples/f24.ogg",
+    velocity: "forte",
+  },
+  {
+    note: "G1",
+    url: "/samples/f31.ogg",
+    velocity: "forte",
+  },
+  {
+    note: "D2",
+    url: "/samples/f38.ogg",
+    velocity: "forte",
+  },
+  {
+    note: "A2",
+    url: "/samples/f45.ogg",
+    velocity: "forte",
+  },
+  {
+    note: "E3",
+    url: "/samples/f52.ogg",
+    velocity: "forte",
+  },
+  {
+    note: "B3",
+    url: "/samples/f59.ogg",
+    velocity: "forte",
+  },
+  {
+    note: "F#4",
+    url: "/samples/f66.ogg",
+    velocity: "forte",
+  },
+  {
+    note: "C#5",
+    url: "/samples/f73.ogg",
+    velocity: "forte",
+  },
+  {
+    note: "G#5",
+    url: "/samples/f80.ogg",
+    velocity: "forte",
+  },
+  {
+    note: "D#6",
+    url: "/samples/f87.ogg",
+    velocity: "forte",
+  },
+  {
+    note: "A#6",
+    url: "/samples/f94.ogg",
+    velocity: "forte",
+  },
+  {
+    note: "F7",
+    url: "/samples/f101.ogg",
+    velocity: "forte",
+  },
+  {
+    note: "C1",
+    url: "/samples/p24.ogg",
+    velocity: "piano",
+  },
+  {
+    note: "G1",
+    url: "/samples/p31.ogg",
+    velocity: "piano",
+  },
+  {
+    note: "D2",
+    url: "/samples/p38.ogg",
+    velocity: "piano",
+  },
+  {
+    note: "A2",
+    url: "/samples/p45.ogg",
+    velocity: "piano",
+  },
+  {
+    note: "E3",
+    url: "/samples/p52.ogg",
+    velocity: "piano",
+  },
+  {
+    note: "B3",
+    url: "/samples/p59.ogg",
+    velocity: "piano",
+  },
+  {
+    note: "F#4",
+    url: "/samples/p66.ogg",
+    velocity: "piano",
+  },
+  {
+    note: "C#5",
+    url: "/samples/p73.ogg",
+    velocity: "piano",
+  },
+  {
+    note: "G#5",
+    url: "/samples/p80.ogg",
+    velocity: "piano",
+  },
+  {
+    note: "D#6",
+    url: "/samples/p87.ogg",
+    velocity: "piano",
+  },
+  {
+    note: "A#6",
+    url: "/samples/p94.ogg",
+    velocity: "piano",
+  },
+  {
+    note: "F7",
+    url: "/samples/p101.ogg",
+    velocity: "piano",
+  },
+]
---
+++ src/store.ts
@@ -0,0 +1,18 @@
+import { create } from "zustand"
+import { NoteName } from "../../minikeys/dist"
+
+type MiniKeysStore = {
+  noteRange: { low: NoteName; high: NoteName } | undefined
+  activeNotes: (NoteName | undefined)[]
+  setNoteRange: (range: { low: NoteName; high: NoteName }) => void
+  setActiveNotes: (notes?: (NoteName | undefined)[]) => void
+}
+
+export const useNotes = create<MiniKeysStore>((set) => ({
+  noteRange: undefined,
+  activeNotes: [],
+  setNoteRange: (range: { low: NoteName; high: NoteName }) =>
+    set({ noteRange: range }),
+  setActiveNotes: (notes?: (NoteName | undefined)[]) =>
+    set({ activeNotes: notes }),
+}))
