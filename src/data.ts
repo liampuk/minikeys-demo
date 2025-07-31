de171ecf0375e83b33471fb36591e74be2df5899
liampuk <liampiesley@gmail.com>
2025-07-31 22:56:34 +0100

    add base url to paths

--- src/data.ts
+++ src/data.ts
@@ -1,124 +1,126 @@
 import { Sample } from "minikeys"
 
+const BASE_URL = import.meta.env.BASE_URL
+
 export const samples: Sample[] = [
   {
     note: "C1",
-    url: "/samples/f24.ogg",
+    url: `${BASE_URL}/samples/f24.ogg`,
     velocity: "forte",
   },
   {
     note: "G1",
-    url: "/samples/f31.ogg",
+    url: `${BASE_URL}/samples/f31.ogg`,
     velocity: "forte",
   },
   {
     note: "D2",
-    url: "/samples/f38.ogg",
+    url: `${BASE_URL}/samples/f38.ogg`,
     velocity: "forte",
   },
   {
     note: "A2",
-    url: "/samples/f45.ogg",
+    url: `${BASE_URL}/samples/f45.ogg`,
     velocity: "forte",
   },
   {
     note: "E3",
-    url: "/samples/f52.ogg",
+    url: `${BASE_URL}/samples/f52.ogg`,
     velocity: "forte",
   },
   {
     note: "B3",
-    url: "/samples/f59.ogg",
+    url: `${BASE_URL}/samples/f59.ogg`,
     velocity: "forte",
   },
   {
     note: "F#4",
-    url: "/samples/f66.ogg",
+    url: `${BASE_URL}/samples/f66.ogg`,
     velocity: "forte",
   },
   {
     note: "C#5",
-    url: "/samples/f73.ogg",
+    url: `${BASE_URL}/samples/f73.ogg`,
     velocity: "forte",
   },
   {
     note: "G#5",
-    url: "/samples/f80.ogg",
+    url: `${BASE_URL}/samples/f80.ogg`,
     velocity: "forte",
   },
   {
     note: "D#6",
-    url: "/samples/f87.ogg",
+    url: `${BASE_URL}/samples/f87.ogg`,
     velocity: "forte",
   },
   {
     note: "A#6",
-    url: "/samples/f94.ogg",
+    url: `${BASE_URL}/samples/f94.ogg`,
     velocity: "forte",
   },
   {
     note: "F7",
-    url: "/samples/f101.ogg",
+    url: `${BASE_URL}/samples/f101.ogg`,
     velocity: "forte",
   },
   {
     note: "C1",
-    url: "/samples/p24.ogg",
+    url: `${BASE_URL}/samples/p24.ogg`,
     velocity: "piano",
   },
   {
     note: "G1",
-    url: "/samples/p31.ogg",
+    url: `${BASE_URL}/samples/p31.ogg`,
     velocity: "piano",
   },
   {
     note: "D2",
-    url: "/samples/p38.ogg",
+    url: `${BASE_URL}/samples/p38.ogg`,
     velocity: "piano",
   },
   {
     note: "A2",
-    url: "/samples/p45.ogg",
+    url: `${BASE_URL}/samples/p45.ogg`,
     velocity: "piano",
   },
   {
     note: "E3",
-    url: "/samples/p52.ogg",
+    url: `${BASE_URL}/samples/p52.ogg`,
     velocity: "piano",
   },
   {
     note: "B3",
-    url: "/samples/p59.ogg",
+    url: `${BASE_URL}/samples/p59.ogg`,
     velocity: "piano",
   },
   {
     note: "F#4",
-    url: "/samples/p66.ogg",
+    url: `${BASE_URL}/samples/p66.ogg`,
     velocity: "piano",
   },
   {
     note: "C#5",
-    url: "/samples/p73.ogg",
+    url: `${BASE_URL}/samples/p73.ogg`,
     velocity: "piano",
   },
   {
     note: "G#5",
-    url: "/samples/p80.ogg",
+    url: `${BASE_URL}/samples/p80.ogg`,
     velocity: "piano",
   },
   {
     note: "D#6",
-    url: "/samples/p87.ogg",
+    url: `${BASE_URL}/samples/p87.ogg`,
     velocity: "piano",
   },
   {
     note: "A#6",
-    url: "/samples/p94.ogg",
+    url: `${BASE_URL}/samples/p94.ogg`,
     velocity: "piano",
   },
   {
     note: "F7",
-    url: "/samples/p101.ogg",
+    url: `${BASE_URL}/samples/p101.ogg`,
     velocity: "piano",
   },
 ]
